115a89c40dc8400cbd8c131f9bac82e0fdbfec4a
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-24 23:13:33 +0200

    Wyrzucam komentarze z metody

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
 public class Main {
+
+    // TU JEDEN SIE PYTAL....
+    // A NA TO CO MNOZYC ? :P
+    // OJ TO NA CO CHCESZ MOZESZ
+
+    // A TU DRUGI NIE WIEDZIAL
+    // JA NIE WIEM CO JA TU ROBIE :(
+    // Bo jestes kretyn
+
+    // A TEN TU ROZKAZYWAL
+    // WEZ MI STAD WYJDZ!!!!!!!!
+    // NIE WYJDE !!!
+    
     public static void main(String[] args) {
         System.out.println("Hello world");
         pomnóżmyTo(5, 10);
@@ -7,16 +20,5 @@ public class Main {
     private static int pomnóżmyTo(int a, int b) {
         return a*b;
 
-        // TU JEDEN SIE PYTAL....
-        // A NA TO CO MNOZYC ? :P
-        // OJ TO NA CO CHCESZ MOZESZ
-
-        // A TU DRUGI NIE WIEDZIAL
-        // JA NIE WIEM CO JA TU ROBIE :(
-        // Bo jestes kretyn
-
-        // A TEN TU ROZKAZYWAL
-        // WEZ MI STAD WYJDZ!!!!!!!!
-        // NIE WYJDE !!!
     }
 }
