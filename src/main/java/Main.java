06430c7f74c77653b018642636d75ed6ecedf051
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-24 22:53:04 +0200

    Merge to master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         doSmth(5, 10);
-    }q
+    }
 
     private static int doSmth(int a, int b) {
         return a+b;
